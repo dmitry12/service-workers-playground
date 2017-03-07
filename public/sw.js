a8d77670b87bedbb4be5473af3da9507d591fc54
Peter Bregnholm Nielsen <pebn@tv2.dk>
2017-03-07 17:49:54 +0100

    Make sure to fetch proper response, and let's show it actually works

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,9 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-    );
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    isOnline: true,
+  }
+
+  componentWillMount() {
+    this.setState({isOnline: navigator.onLine});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Offline!</h2>
+          <h2>Welcome to {this.state.isOnline ? 'Online' : 'Offline'}!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
